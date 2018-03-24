3eb2b2c2d86ae7200191f53ad09d2ea6b888cf08
Michał Rychlewicz <michal.rychlewicz@edu.uekat.pl>
2018-03-24 12:22:01 +0100

    Initial commit

---
+++ src/main/java/pl/sternik/mr/zadania/spring/zad09/RunIt.java
@@ -0,0 +1,29 @@
+package pl.sternik.mr.zadania.spring.zad09;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//import MyFirstBean;
+//import MySecondBean;
+//import MyThirdBean;
+
+public class RunIt {
+	public static void main(String[] args) {
+//		AnnotationConfigApplicationContext ctx = 
+//		        new AnnotationConfigApplicationContext(
+//				BeanConfigurator.class);
+//
+//		MySecondBean obj2 = (MySecondBean) ctx.getBean("myDrugi");
+//		System.out.println(obj2.getName());
+//
+//		MySecondBean obj3 = (MySecondBean) ctx.getBean("myDrugiNazwany");
+//		System.out.println(obj3.getName());
+//		
+//		MyThirdBean obj4 = ctx.getBean("mySkomplikowany",MyThirdBean.class);
+//		System.out.println(obj4.getCompoundName());
+//		
+//		ctx.close();
+
+	}
+}
